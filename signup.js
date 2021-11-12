fb420c69bd392ddedea3ad553c5b564c6aabe846
Nithin Narayanan <75129591+ryn018@users.noreply.github.com>
2021-11-12 21:37:52 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,36 @@
+<script>
+        let email = document.getElementById("email");
+        let error =document.getElementById("emailv");
+        let password = document.getElementById("password")
+        let errorpass= document.getElementById("pass");
+     
+      function validate() {
+
+
+        function validate(){
+        let regexemail=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+        let regexpasswrd=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        let e,p;
+    if(regexemail.test(email.value)){
+        e=true;
+    }else{
+        error.innerHTML= "Invalid";
+        error.style.color="red";
+        }
+    if(regexpasswrd.test(password.value)){
+        p = true;
+    }else{
+        errorpass.innerHTML= "Invalid";
+        errorpass.style.color="red";
+       
+    }
+    if(e===true&&p===true){
+        return true;
+    }else{
+        return false;
+    }
+}
+}
+      
+
+    </script>
